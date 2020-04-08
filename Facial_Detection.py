f452c43bdd30e29b83a6940ebee41e8c5c7ef020
mdatz <kadatzmatthew@gmail.com>
2020-04-07 22:37:04 -0600

    Updated so openCV can handle .pgm photo as input

--- Facial_Detection.py
+++ Facial_Detection.py
@@ -1,2 +1,16 @@
 ##ENCM 509 - Lab Project
 ##CNN Facial Recognizer
+
+import cv2
+import dlib
+import scipy
+import numpy
+import os
+
+print("Testing Image Read of .pgm file")
+test = cv2.imread('./images/yaleB01/yaleB01_P00A+000E+00.pgm')
+
+cv2.imshow('Test Image',test)
+if cv2.waitKey():
+    print("Success")
+    exit()
