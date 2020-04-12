5277e3e36973c066586dbea91ae738640a363f36
mdatz <kadatzmatthew@gmail.com>
2020-04-12 14:48:00 -0600

    Hi

--- Facial_Detection.py
+++ Facial_Detection.py
@@ -7,10 +7,21 @@ import scipy
 import numpy
 import os
 
-print("Testing Image Read of .pgm file")
-test = cv2.imread('./images/yaleB01/yaleB01_P00A+000E+00.pgm')
+##Load Needed Face Detection Models
+face_detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+face_recognizer = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
 
-cv2.imshow('Test Image',test)
-if cv2.waitKey():
-    print("Success")
-    exit()
+##Set Threshold Value for Matches
+tolerance = 0.5
+
+##Read Face Images
+image = cv2.imread('./images/yaleB01/yaleB01_P00A+000E+00.pgm')
+
+##Detect Face and Face Orientation/Shape
+detected_faces = face_detector(image,1)
+shapes_faces = [shape_predictor(image,face) for face in detected_faces]
+
+##Determine Encoding For Given Face
+encoding = [numpy.array(face_recognizer.compute_face_descriptor(image, face_pose, 1)) for face_pose in shapes_faces]
+print(encoding)
