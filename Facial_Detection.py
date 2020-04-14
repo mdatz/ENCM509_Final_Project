143ef0597d86bf21fa8b243f26f416c2fbe3967d
pmatchett <pmatchett@shaw.ca>
2020-04-13 18:24:17 -0600

    matching for users implemented

--- Facial_Detection.py
+++ Facial_Detection.py
@@ -16,20 +16,25 @@ face_recognizer = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_m
 tolerance = 0.03
 
 ##DataBase Values & Feature Vector List
-db_count = 39
+db_count = 40
 db_encodings = []
-num_samples = 10
+sample_start = 1
+num_samples = 6
+query_sample_start = 11
+query_sample_end = 16
 
 ##Read Images and Store Model Encodings into DataBase
-for i in range(1, db_count):
+for i in range(sample_start, db_count):
+    user_encodings = []
     
     ##Missing Training Sample :(
     if i == 14:
+        db_encodings.append(user_encodings)
         continue
         
     for j in range(1,num_samples):
         ##Create File String & Read Image
-        file_str = "./images/yaleB%02d/yaleB%02d (%1d).pgm" % (i,i,j)
+        file_str = "./images/yaleB%02d/yaleB%02d (%d).pgm" % (i,i,j)
         print("Reading Image: " + file_str)
         image = cv2.imread(file_str)
 		
@@ -42,22 +47,55 @@ for i in range(1, db_count):
         if encoding == []:
             print("Image %d Failed" % (i))
         else:  
-            db_encodings.append(encoding)
+            user_encodings.append(encoding)
+    db_encodings.append(user_encodings)
 print("DataBase Created!")
 
-##Test a Query Image Against DataBase Encodings
-query_image = cv2.imread("./images/yaleB03/yaleB03 (2).pgm")
-
-##Get Query Image Encoding
-detected_faces = face_detector(query_image,1)
-shapes_faces = [shape_predictor(query_image,face) for face in detected_faces]
-query_encoding = [numpy.array(face_recognizer.compute_face_descriptor(query_image, face_pose, 1)) for face_pose in shapes_faces]
-
-##Check Difference from DB Images
-for i in range(len(db_encodings)):
+FA_count = 0
+FR_count = 0
+num_tests = 0
+matched = 0
+#for every user
+for j in range(1, db_count):
+    if j ==14:
+        continue
+    #for entries 6-10	
+    for k in range(query_sample_start, query_sample_end):
+        ##Test a Query Image Against DataBase Encodings
+        query_file = "./images/yaleB%02d/yaleB%02d (%d).pgm" % (j,j,k)
+        query_image = cv2.imread(query_file)
+        print("Testing image %d from user %d" % (k,j))
+        ##Get Query Image Encoding
+        detected_faces = face_detector(query_image,1)
+        shapes_faces = [shape_predictor(query_image,face) for face in detected_faces]
+        query_encoding = [numpy.array(face_recognizer.compute_face_descriptor(query_image, face_pose, 1)) for face_pose in shapes_faces]
+        if query_encoding == []:
+            print("Query image failed to load")
+            continue
+        ##Check Difference from DB Images
+		#for every user
+        for i in range(len(db_encodings)):
+            if i == 14:
+                continue
+            matched = 0
+		    #for every entry from that user
+            for q in range(len(db_encodings[i])):
     
-    diff = abs(numpy.subtract(query_encoding, db_encodings[i]))
-    score = numpy.average(diff)
+                diff = abs(numpy.subtract(query_encoding, db_encodings[i][q]))
+                score = numpy.average(diff)
+				
+                num_tests+=1
         
-    if score <= tolerance:
-        print("Possible match with DB Entry #%d - Score:%.3f" % ((i+1),score))
+                if score <= tolerance:
+                    print("Possible match with DB User #%d - Score:%.3f" % ((i+1),score))
+                    matched = 1
+					##checking for false accepts
+                    if i!=j:
+                        FA_count+=1
+			##checking for false rejects
+            if matched == 0 and i==j:
+                FR_count+=1
+print("Total number of false rejections: %d" %(FR_count))
+print("Total number of false acceptions: %d" %(FA_count))
+print("FRR: %.4f" %(FR_count/num_tests))
+print("FAR: %.4f" %(FA_count/num_tests))
